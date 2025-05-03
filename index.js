111f7996a819fcdacc27a782cc0e35a8c0837651
Divyanshu Pandey <divyanshupandey9329@gmail.com>
2025-05-04 04:05:21 +0530

    Initial Author Route

--- index.js
+++ index.js
@@ -8,7 +8,10 @@ const expresslayout = require("express-ejs-layouts");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const bodyParser = require("body-parser");
+
 const indexRouter = require("./routes/index");
+const authorRouter = require("./routes/authers");
 
 // Set the view engine to ejs
 app.set("view engine", "ejs");
@@ -16,6 +19,7 @@ app.set("views", "./views");
 app.set("layout", "./layouts/layout");
 app.use(expresslayout);
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({limit:'10mb', extended: false }));
 
 const mongoose = require("mongoose");
 mongoose.connect(process.env.DATABASE_URL);
@@ -24,6 +28,7 @@ db.on("error", error => console.error(error));
 db.once("open", () => console.log("Connected to Mongoose"));
 
 app.use('/', indexRouter);
+app.use('/authors', authorRouter);
 
 // app.listen(PORT, ()=> console.log(`Server is running on PORT ${PORT}`));
 app.listen(process.env.PORT || 3000, () => {
